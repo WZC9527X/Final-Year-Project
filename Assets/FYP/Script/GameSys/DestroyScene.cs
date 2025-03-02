33a4accace1be9761d4610de8ac0caa00937c944
WZC9527X <143982821+WZC9527X@users.noreply.github.com>
2025-03-02 18:57:53 +0800

    LoopScene

---
+++ Assets/FYP/Script/GameSys/DestroyScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject.transform.parent.parent.gameObject);
+
+        }
+
+
+    }
+}
---
+++ Assets/FYP/Script/GameSys/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool hideMouse;
+
+    public static GameManager Instance = null;
+
+    void Start()
+    {
+        if (hideMouse)
+        {
+            MouseControl(false);
+        }
+        else
+        {
+            MouseControl(true);
+        }
+
+    }
+
+    void Update()
+    {
+       
+
+        
+    }
+
+    public void MouseControl(bool YN)
+    {
+        if (YN)
+        {
+            
+            //Display Mouse
+            //MouseControl(true);
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+
+        }
+        else
+        {
+            //Hide Mouse
+            //MouseControl(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+
+    }
+
+    
+    
+   
+
+    
+
+}
