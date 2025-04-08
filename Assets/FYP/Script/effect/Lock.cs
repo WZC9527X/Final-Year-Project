e8ad5ddf8ce2a148c09ea896cde3e7783dcb50f7
Nicogging <230164168@stu.vtc.edu.hk>
2025-04-08 14:44:03 +0800

    Lock v0.2
    Scene will change color
    Add sound
    Fix bug

--- Assets/FYP/Script/effect/Lock.cs
+++ Assets/FYP/Script/effect/Lock.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Lock : MonoBehaviour
 {
     private bool PlayerLock = false;
     private PlayerMovement PlayerMovement;
+    public Image blackImage;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        if (blackImage != null)
+        {
+            blackImage.color = new Color(0, 0, 0, 0);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -23,12 +34,33 @@ public class Lock : MonoBehaviour
     {
         PlayerLock = true;
         PlayerMovement.DisableMovement();
-        PlayerMovement.LockCamera();
+        yield return StartCoroutine(FadeToBlack(1f));
+        audioSource.Play();
+        //PlayerMovement.LockCamera();
 
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(7.5f);
+
+        yield return StartCoroutine(FadeToBlack(0f));
 
         PlayerMovement.EnableMovement();
-        PlayerMovement.UnlockCamera();
+        //PlayerMovement.UnlockCamera();
         //PlayerLock = false;
+    }
+
+    private IEnumerator FadeToBlack(float targetAlpha)
+    {
+        float duration = 1f;
+        float startAlpha = blackImage.color.a;
+        float time = 0;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, targetAlpha, time / duration);
+            blackImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        blackImage.color = new Color(0, 0, 0, targetAlpha);
     }
 }
