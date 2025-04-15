40b90865557dcf8881a8504487a93baee5676199
Nicogging <230164168@stu.vtc.edu.hk>
2025-04-15 14:32:11 +0800

    Footsteps Sound
    Walking & Running
    Change the walking & running speed

---
+++ Assets/FYP/Script/Audio/Footsteps.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioSource Walking , Running;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.D))
+        {
+            Walking.enabled = true;
+
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                Walking.enabled = false;
+                Running.enabled = true;
+            }else{
+                Running.enabled = false;
+            }
+        }else{
+            Walking.enabled = false;
+            Running.enabled = false;
+        }
+    }
+}
