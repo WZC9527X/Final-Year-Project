199b9f7a5bd420adda8fbb8f5569e1027bf4fe0d
Nicogging <230164168@stu.vtc.edu.hk>
2025-04-22 21:24:18 +0800

    Lock v 0.3

---
+++ Assets/FYP/Script/effect/JumpTrigger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTrigger : MonoBehaviour
+{
+    public AudioSource Scream;
+    public GameObject ThePlayer;
+    public GameObject JumpCam;
+    public GameObject FlashImg;
+    private bool hasJumped = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!hasJumped && other.gameObject == ThePlayer)
+        {
+            hasJumped = true;
+            Scream.Play();
+            JumpCam.SetActive(true);
+            ThePlayer.SetActive(false);
+            FlashImg.SetActive(true);
+            StartCoroutine(EndJump());
+
+        }
+
+    }
+    
+
+    IEnumerator EndJump()
+    {
+        yield return new WaitForSeconds(2.03f);
+        ThePlayer.SetActive(true);
+        JumpCam.SetActive(false);
+        FlashImg.SetActive(false);
+    }
+
+}
+
+
