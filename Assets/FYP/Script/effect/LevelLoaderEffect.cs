09ba8776cd2e7d99be17cbcf6a4f843bfc2e6412
WZC9527X <143982821+WZC9527X@users.noreply.github.com>
2025-05-09 18:19:51 +0800

    static scene

--- Assets/FYP/Script/effect/LevelLoaderEffect.cs
+++ Assets/FYP/Script/effect/LevelLoaderEffect.cs
@@ -4,18 +4,36 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoaderEffect : MonoBehaviour
 {
-    public Animator transition;
+    public GameObject _LevelLoaderCanvas;
+
+    Animator transition;
+    CanvasGroup _CanvasGroup;
     public static Animator _transition;
 
-    private void Awake()
-    {
-        _transition = transition;
-    }
+    public bool _EnableLevelLoaderEffect = false;
 
+
+    private void Start()
+    {
+        transition = _LevelLoaderCanvas.GetComponent<Animator>();
+        _CanvasGroup = _LevelLoaderCanvas.GetComponent<CanvasGroup>();
+
+        _transition = transition;
+
+        if (_EnableLevelLoaderEffect)
+        {
+            _CanvasGroup.alpha = 1f;
+        }
+        else
+        {
+            _CanvasGroup.alpha = 0f;
+        }
+    }
 
     public static IEnumerator LoadLevel(int levelIndex)
     {
         _transition.SetTrigger("LevelLoader_Start");
+
         yield return new WaitForSeconds(1.5f);
 
         // <------add loading log
